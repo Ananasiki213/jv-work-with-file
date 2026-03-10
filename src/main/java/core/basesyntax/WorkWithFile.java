5b6b745e22ce747fb360c4c9918a4da95dbc1929
Super <meizum3notewh@gmail.com>
2026-03-10 18:33:33 +0200

    Done!:)

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -7,26 +7,30 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class WorkWithFile {
-
     public void getStatistic(String fromFileName, String toFileName) {
-    int supplySum = 0;
-    int buysum = 0;
+        int supplySum = 0;
+        int buySum = 0;
 
-    try {
-        List<String> lines = Files.readAllLines(Paths.get(fromFileName));
-        for (String line : lines) {
-        String[] splitLine = line.split(",");
-        int amount = Integer.parseInt(splitLine[1]);
-        if (splitLine[0].equals("supply")) {
-            supplySum += amount;
-        }else if (splitLine[0].equals("buy")) {
-            buysum += amount;
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(fromFileName));
+            for (String line : lines) {
+                String[] splitLine = line.split(",");
+                int amount = Integer.parseInt(splitLine[1]);
+
+                if (splitLine[0].equals("supply")) {
+                    supplySum += amount;
+                } else if (splitLine[0].equals("buy")) {
+                    buySum += amount;
+                }
+            }
+
+            int result = supplySum - buySum;
+            String report = "supply," + supplySum + System.lineSeparator()
+                    + "buy," + buySum + System.lineSeparator()
+                    + "result," + result;
+            Files.writeString(Path.of(toFileName), report);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't handle files", e);
         }
-    } int result = supplySum - buysum;
-        String report = "supply," + supplySum + "\n" + buysum + "\n" + result;
-        Files.writeString(Path.of(toFileName), report);
-    } catch (IOException e) {
-        throw new RuntimeException("Can't handle files", e);
-    }
     }
 }
