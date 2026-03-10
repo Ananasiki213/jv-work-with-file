c0a3f6f958da13628040bf904efbc1bc16019371
Super <meizum3notewh@gmail.com>
2026-03-10 18:30:43 +0200

    Done!:)

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,32 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
+public class WorkWithFile {
+
+    public void getStatistic(String fromFileName, String toFileName) {
+    int supplySum = 0;
+    int buysum = 0;
+
+    try {
+        List<String> lines = Files.readAllLines(Paths.get(fromFileName));
+        for (String line : lines) {
+        String[] splitLine = line.split(",");
+        int amount = Integer.parseInt(splitLine[1]);
+        if (splitLine[0].equals("supply")) {
+            supplySum += amount;
+        }else if (splitLine[0].equals("buy")) {
+            buysum += amount;
+        }
+    } int result = supplySum - buysum;
+        String report = "supply," + supplySum + "\n" + buysum + "\n" + result;
+        Files.writeString(Path.of(toFileName), report);
+    } catch (IOException e) {
+        throw new RuntimeException("Can't handle files", e);
+    }
     }
 }
